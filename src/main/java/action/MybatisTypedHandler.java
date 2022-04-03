e6e871bccf8b54663f3761ca35c223cf4cdd4443
BigCatLi <lidacat@163.com>
2022-04-03 15:14:53 +0800

    Create test Action

---
+++ src/main/java/action/MybatisTypedHandler.java
@@ -0,0 +1,15 @@
+package action;
+
+import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
+
+/**
+ * <p></p>
+ *
+ * @author: bigcatlee
+ * @date: 2022/4/2 12:58
+ * @description:
+ */
+
+public class MybatisTypedHandler extends TypedHandlerDelegate {
+
+}
